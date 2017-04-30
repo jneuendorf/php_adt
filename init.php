d1ada6b709b32505ba6f00b65bc3c7250e9f44b2
Jim Neuendorf <jim.neuendorf@gmx.de>
2017-04-30 16:39:57 +0200

    added composer.json and license

--- init.php
+++ init.php
@@ -14,5 +14,14 @@ require_once 'Dict.php';
 require_once 'Genewrapor.php';
 require_once 'Set.php';
 require_once 'Tree.php';
+// TODO: don't use `set_include_path` at all
+// TODO: support PSR-4 autoloading?
+// require_once __DIR__.DIRECTORY_SEPARATOR.'_php_adt/init.php';
+// require_once __DIR__.DIRECTORY_SEPARATOR.'Arr.php';
+// require_once __DIR__.DIRECTORY_SEPARATOR.'CharArr.php';
+// require_once __DIR__.DIRECTORY_SEPARATOR.'Dict.php';
+// require_once __DIR__.DIRECTORY_SEPARATOR.'Genewrapor.php';
+// require_once __DIR__.DIRECTORY_SEPARATOR.'Set.php';
+// require_once __DIR__.DIRECTORY_SEPARATOR.'Tree.php';
 
 set_include_path($orig_include_path);
